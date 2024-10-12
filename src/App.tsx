3ef6b5117ddbaefd2f17167fb2c604d75027be8b
George Boktor <gsboktor@gmail.com>
2024-10-12 12:34:15 -0500

    clean up timeouts

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,39 @@ import { useRampCTFGetFlag } from './hooks';
 
 function App() {
   const [flag, setFlag] = useState<string[]>();
+  const [timeouts, setTimeouts] = useState<NodeJS.Timeout[]>([]);
 
   const {value: getFlagStateValue, loading: getFlagStateLoading} = useRampCTFGetFlag();
 
-
   const handleSetFlagWithTypewriterEffect = useCallback((flag: string) => {
+    const newTimeouts: NodeJS.Timeout[] = [];
+
     flag.split("").forEach((char, index) => {
-      setTimeout(() => {
+      const timeout = setTimeout(() => {
         setFlag(prev => [...(prev ?? []), char]);
       }, index * 500);
+
+      newTimeouts.push(timeout);
     });
+
+    setTimeouts(newTimeouts);
   }, []);
 
   useEffect(() => {
     if (getFlagStateValue) {
       handleSetFlagWithTypewriterEffect(getFlagStateValue);
     }
+    return () => {
+      timeouts.forEach(clearTimeout);
+    };
   }, [getFlagStateValue, handleSetFlagWithTypewriterEffect]);
-
-
 
   return (
     <div style={{ backgroundColor: "lightgray", position: "absolute", width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      {getFlagStateLoading ? (<p>Loading...</p>) :
+      {getFlagStateLoading ? (<p style={{fontSize: "2rem"}}>Loading...</p>) :
         (<div style={{ display: "flex", flexDirection: "row" }}>
           {flag?.map((char, index) => (
-            <p key={index}>{char}</p>
+            <p key={index} style={{fontSize: "2rem"}}>{char}</p>
           ))}
         </div>)
       }
